d36b62cb0f24807b22e8a0d94cfdca7781e97bc4
nlj <nlj@PC>
2017-12-07 23:31:22 +0800

    commit

---
+++ ssoclient/src/main/java/cn/com/nlj/sso/client/SsoclientApplication.java
@@ -0,0 +1,12 @@
+package cn.com.nlj.sso.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoclientApplication.class, args);
+	}
+}
---
+++ ssoservice/src/main/java/cn/com/nlj/sso/service/SsoserviceApplication.java
@@ -0,0 +1,12 @@
+package cn.com.nlj.sso.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoserviceApplication.class, args);
+	}
+}
